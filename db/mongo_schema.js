84a7b3883fefa2d7f106169c7be4ee3747f40343
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-16 15:05:49 -0700

    add constrain to mongo db

--- db/mongo_schema.js
+++ db/mongo_schema.js
@@ -2,31 +2,31 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let stylesSchema = mongoose.Schema({
-  style_id: Number,
-  name: String,
-  original_price: String,
+  style_id: { type: Number, required: true },
+  name: { type: String, required: true },
+  original_price: { type: String, required: true },
   sale_price: String,
-  "default?": Boolean,
+  "default?": { type: Boolean, required: true },
   photos: [{
     thumbnail_url: String,
     url: String
     }],
   skus: [{
-    sku:String,
-    quantity: Number,
-    size: String
+    sku: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    size: { type: String, required: true }
   }]
 });
 
 let productsSchema = mongoose.Schema({
-  id: { type: Number, unique: true, dropDups: true},
-  name: String,
-  slogan: String,
-  description: String,
-  category: String,
-  default_price: String,
+  id: { type: Number, unique: true, dropDups: true, required: true },
+  name: { type: String, required: true },
+  slogan: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: String, required: true },
+  default_price: { type: String, required: true },
   features: [{
-    feature: String,
+    feature: { type: String, required: true },
     value: String
     }],
   related: [Number],
