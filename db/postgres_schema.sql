05ea0ca04c0ec2f59dadb836b9ee9fc090f6f248
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-16 14:59:01 -0700

    add constrain to postgres schema

--- db/postgres_schema.sql
+++ db/postgres_schema.sql
@@ -1,36 +1,35 @@
 DROP TABLE IF EXISTS product;
 CREATE TABLE products (
-  "id"            SERIAL PRIMARY KEY,
-  "name"          varchar(50),
-  "slogan"        varchar(100),
-  "description"   varchar(300),
-  "category"      varchar(50),
-  "default_price" real,
-  "related"       JSON
+  "id"            SERIAL       PRIMARY KEY,
+  "name"          varchar(50)  NOT NULL,
+  "slogan"        varchar(100) NOT NULL,
+  "description"   varchar(300) NOT NULL,
+  "category"      varchar(50)  NOT NULL,
+  "default_price" varchar(50)  NOT NULL,
 );
 
 DROP TABLE IF EXISTS features;
 CREATE TABLE features (
-  "product_id"  INT NOT NULL references products(id),
-  "feature"     varchar(50),
+  "product_id"  INT         NOT NULL references products(id),
+  "feature"     varchar(50) NOT NULL,
   "value"       varchar(50)
 );
 
 DROP TABLE IF EXISTS related;
 CREATE TABLE related (
   "product_id"        INT NOT NULL references products(id),
-  "related_product"   INT
+  "related_product"   INT NOT NULL
 );
 
 DROP TABLE IF EXISTS styles;
 CREATE TABLE styles (
-  "id"                SERIAL PRIMARY KEY,
-  "product_id"        INT  NOT NULL references products(id),
-  "style_id"          INT,
-  "name"              varchar(50),
-  "original_price"    real,
-  "sale_price"        real,
-  "default?"          boolean
+  "id"                SERIAL      PRIMARY KEY,
+  "product_id"        INT         NOT NULL references products(id),
+  "style_id"          INT         NOT NULL,
+  "name"              varchar(50) NOT NULL,
+  "original_price"    varchar(50) NOT NULL,
+  "sale_price"        varchar(50),
+  "default?"          boolean     NOT NULL
 );
 
 DROP TABLE IF EXISTS photos;
@@ -43,11 +42,11 @@ CREATE TABLE photos (
 
 DROP TABLE IF EXISTS skus;
 CREATE TABLE skus (
-  "product_id"    INT NOT NULL references products(id),
-  "style_id"      INT NOT NULL references styles(id),
-  "sku_id"        varchar(50),
-  "quantity"      INT,
-  "size"          varchar(10)
+  "product_id"    INT         NOT NULL references products(id),
+  "style_id"      INT         NOT NULL references styles(id),
+  "sku_id"        varchar(50) NOT NULL,
+  "quantity"      INT         NOT NULL,
+  "size"          varchar(10) NOT NULL
 );
 
 
