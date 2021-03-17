a41a4b55667edbf3830248574858c76804efa0cc
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-16 17:47:10 -0700

    add mongoose module before schema

--- db/mongo_schema.js
+++ db/mongo_schema.js
@@ -35,12 +35,11 @@ let productsSchema = mongoose.Schema({
 
 let usersSchema = mongoose.Schema({
   id: { type: Number, unique: true, dropDups: true, required: true },
-  name: { type: String, required: true },
 });
 
 let cartsSchema = mongoose.Schema({
   id: { type: Number, unique: true, dropDups: true, required: true },
-  user_id: {type: Schema.Types.ObjectId, ref: 'usersSchema'},
+  user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'usersSchema'},
   sku_id: { type: String, required: true },
   count: { type: Number, required: true },
 });
