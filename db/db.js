91a5812da88a3c78157905644edbd173509de0b5
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-23 12:17:36 -0700

    local docker environment

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const conn = mongoose.connect('mongodb://localhost/products', { useCreateIndex: true });
+const conn = mongoose.connect('mongodb://mongodb/products', { useCreateIndex: true });
 
 let stylesSchema = mongoose.Schema({
   style_id: { type: Number, required: true },
