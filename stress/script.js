c51ebcca68271f066f3bb2fdcc6d85f3cabbd40d
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-26 02:58:32 -0700

    upscale nginx processer setting

--- stress/script.js
+++ stress/script.js
@@ -2,10 +2,10 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 export let options = {
   stages: [
-    { duration: '1s', target: 1000 },
-    { duration: '61s', target: 1000 },
+    { duration: '10s', target: 1000 },
+    { duration: '60s', target: 1000 },
   ],
-  // vus: 1000,
+  // vus: 500,
   // duration: '5s',
 };
 let count = 0;
@@ -26,7 +26,7 @@ export default function () {
   // get one product information
   // const id = 1 + Math.floor(Math.random() * 10000);
   const id = 14034;
-  let res = http.get(`http://localhost:300${(count++)%2}/products/${id}/related`);
+  let res = http.get(`http://localhost:8080/products/${id}/related`);
   // let res = http.get(`http://localhost:3000/products/${id}/related`);
   check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
